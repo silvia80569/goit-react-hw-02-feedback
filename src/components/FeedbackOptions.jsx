b8c47076b5de45e5d002ca48e2d2553ba1bc5daa
silvia80569 <157542289+silvia80569@users.noreply.github.com>
2024-09-07 23:54:36 +0200

    Changes

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -4,21 +4,21 @@ import styles from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <div>
-    {options.map((option) => (
-      <button key={option}
-         className={styles.button}
-        onClick={() => onLeaveFeedback(option)}>
+    {options.map(option => (
+      <button
+        key={option}
+        className={styles.button}
+        onClick={() => onLeaveFeedback(option)}
+      >
         {option}
       </button>
     ))}
   </div>
 );
 
-
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
-
 
 export default FeedbackOptions;
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -10,7 +10,7 @@ const Section = ({ title, children }) => (
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
-  children:PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired,
+};
 
-export default Section
+export default Section;
